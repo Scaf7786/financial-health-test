7dfcfbe878638243c17d4590f50ca1762b629c67
Scaf7786 <stas.omg@gmail.com>
2025-02-11 15:34:33 +0600

    Update send-to-google.js

--- api/send-to-google.js
+++ api/send-to-google.js
@@ -1,14 +1,24 @@
+import fs from 'fs';
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         try {
             const { email, gender, age, country, score } = req.body;
 
-            // Отправляем данные в Google Apps Script
-            await fetch('https://script.google.com/macros/s/AKfycbxvNsWQpyhvMwC_aL4Yrqgz-82bBm1yPy9ENWR_nvO5NowakmUfYaw8mPpO929rqhY3cQ/exec', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, gender, age, country, score })
-            });
+            // Чтение существующих данных из файла
+            let data = [];
+            try {
+                const fileContent = await fs.promises.readFile('data.json', 'utf8');
+                data = JSON.parse(fileContent);
+            } catch (err) {
+                console.log('Файл не найден, создаем новый...');
+            }
+
+            // Добавляем новые данные
+            data.push({ email, gender, age, country, score });
+
+            // Записываем обновленные данные в файл
+            await fs.promises.writeFile('data.json', JSON.stringify(data, null, 2));
 
             res.status(200).send('OK');
         } catch (error) {
